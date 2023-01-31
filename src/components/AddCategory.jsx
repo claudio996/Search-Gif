92d259caca8991ac70683ea8ef66aecf18b4e606
Claudio996 <claudioantonio.retamal@gmail.com>
2023-01-31 15:51:02 -0300

    INCLUDE FORM IN COMPONENT AND PREVENT COMPORT DEFAULT

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -2,20 +2,28 @@ import React, { useState } from "react";
 
 const addCategory = () => {
   //export function for comunication with father component
-  const [InputValue, setInputValue] = useState('VALUE-1')
+  const [InputValue, setInputValue] = useState("VALUE-1");
 
   const onInputChange = (event) => {
     const value = event.target.value;
     setInputValue(value);
   };
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(InputValue.trim().length <= 1) return;
+    
+  };
+
   return (
-    <input
-      type="text"
-      placeholder="Your browser gif"
-      value={InputValue}
-      onChange={(event) => onInputChange(event)}
-    />
+    <form onSubmit={(event) => onSubmit(event)}>
+      <input
+        type="text"
+        placeholder="Your browser gif"
+        value={InputValue}
+        onChange={(event) => onInputChange(event)}
+      />
+    </form>
   );
 };
 
